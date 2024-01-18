65da779f7702059f1282aa9c353db7df0125006a
jagjeet <sappaljagjeet@gmail.com>
2024-01-18 20:18:29 +0530

    addes some section

--- client/src/pages/FindMentor/FindMentor.jsx
+++ client/src/pages/FindMentor/FindMentor.jsx
@@ -1,6 +1,7 @@
 import { Drawer } from "@material-tailwind/react";
-
+import { useNavigate } from "react-router-dom"
 const FindMentor = () => {
+  const navigate=useNavigate();
   return (
     <div>
       <section className="bg-white dark:bg-gray-900">
@@ -100,6 +101,7 @@ const FindMentor = () => {
                     <button
                 type="button"
                 className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-primary rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 bg-blue-700 hover:bg-blue-800 text-white "
+              onClick={()=>navigate("/Profile")}
               >
                 View Profile
               </button>
